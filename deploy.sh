f28aefc96a6b6ea7a525891f7fae4ab5b5b5c630
Odin <odinuv@gmail.com>
2017-08-30 09:07:29 +0200

    fix: image tags

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 set -e
 
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag docs-example-logging-python docs-example-logging-python:$TRAVIS_TAG
-docker tag docs-example-logging-python docs-example-logging-python:latest
+docker tag docs-example-logging-python quay.io/keboola/docs-example-logging-python:$TRAVIS_TAG
+docker tag docs-example-logging-python quay.io/keboola/docs-example-logging-python:latest
 docker images
 docker push quay.io/keboola/docs-example-logging-python:$TRAVIS_TAG
 docker push quay.io/keboola/docs-example-logging-python:latest
